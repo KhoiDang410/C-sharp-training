c95789326ebe982eb1b029ecb11c5d20d3dfbb21
Ngoc Khoi Dang <khoidnyds@gmail.com>
2018-03-23 12:13:49 -0400

    data

---
+++ Generic/Collectlt/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collectlt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<Employee> employees = new List<Employee>
+            //{
+            //    new Employee{Name="Scott"},
+            //    new Employee{Name="Alex"}
+            //};
+            //employees.Add(new Employee { Name = "Dani" });
+            //foreach (var employee in employees)
+            //{
+            //    Console.WriteLine(employee.Name);
+            //}
+            //for (int i  = 0; i  < employees.Count; i ++)
+            //{
+            //    Console.WriteLine(employees[i].Name);
+            //}
+
+            //FIFO data structure
+            //Queue<Employee> line = new Queue<Employee>();
+            //line.Enqueue(new Employee { Name = "Alex" });
+            //line.Enqueue(new Employee { Name = "Dani" });
+            //line.Enqueue(new Employee { Name = "Chris" });
+            //while (line.Count > 0)
+            //{
+            //    var employee = line.Dequeue(); ;
+            //    Console.WriteLine(employee.Name);
+            //}
+
+            //LIFO data structure
+            //Stack<Employee> stack = new Stack<Employee>();
+            //stack.Push(new Employee { Name = "Alex" });
+            //stack.Push(new Employee { Name = "Dani" });
+            //stack.Push(new Employee { Name = "Chris" });
+            //while (stack.Count > 0)
+            //{
+            //    var employee = stack.Pop(); ;
+            //    Console.WriteLine(employee.Name);
+            //}
+
+            //HashSet<int> set = new HashSet<int>();
+            //set.Add(1);
+            //set.Add(2);
+            //set.Add(2);
+            //foreach (var item in set)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //LinkedList<int> list = new LinkedList<int>();
+            //list.AddFirst(2);
+            //list.AddFirst(3);
+            //var first = list.First;
+            //list.AddAfter(first, 5);
+            //list.AddBefore(first, 10);
+
+            //foreach (var item in list)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            Dictionary<string,List<Employee>> employeesByName = new Dictionary<string,List<Employee>>();
+            employeesByName.Add("Scott",
+                new List<Employee> { new Employee { Name = "Scott" } });
+
+
+            employeesByName["Scott"].Add(new Employee { Name = "Scott" });
+
+            foreach (var item in employeesByName)
+            {
+                foreach (var employee in item.Value)
+                {
+                    Console.WriteLine(employee.Name);
+
+                }
+            }
+        }
+    }
+}
+
--- OdeToFood/OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/OdeToFood/Controllers/HomeController.cs
@@ -9,14 +9,29 @@ namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        OdeToFoodDb _db = new OdeToFoodDb();
+        public ActionResult Index(string searchTerm=null)
         {
-            var controller = RouteData.Values["controller"];
-            var action = RouteData.Values["action"];
-            var id = RouteData.Values["id"];
-            var message = String.Format($"{controller}::{action} {id}");
-            ViewBag.Message = message;
-            return View();
+            //var controller = RouteData.Values["controller"];
+            //var action = RouteData.Values["action"];
+            //var id = RouteData.Values["id"];
+            //var message = String.Format($"{controller}::{action} {id}");
+            //ViewBag.Message = message;
+
+            var model =
+                _db.Restaurants
+                .OrderByDescending(r=>r.Reviews.Average(review=>review.Rating))
+                .Where(r=>searchTerm==null||r.Name.StartsWith(searchTerm))
+                .Take(10)
+                .Select (r=>new RestaurantListViewModel{
+                    Id=r.Id,
+                    Name=r.Name,
+                    City=r.City,
+                    Country=r.Country,
+                    CountOfReviews = r.Reviews.Count()
+                });
+
+            return View(model);
         }
 
         public ActionResult About()
@@ -33,6 +48,14 @@ namespace OdeToFood.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if(_db!=null)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
